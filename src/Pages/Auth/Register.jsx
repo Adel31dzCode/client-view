550927c650105904fd1cac60f4eee54dd9447813
Adel31_dz <132948186+Adel31dzCode@users.noreply.github.com>
2025-09-03 02:07:50 +0100

    Update Register.jsx

--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -9,8 +9,6 @@ import { useNavigate } from 'react-router-dom';
 
 
 export default function Register({ onSubmit }) {
-
-  const auth = getAuth();
   const [form, setForm] = useState({
     name: '',
     email: '',
@@ -204,3 +202,4 @@ const handleSubmit = async (e) => {
     </>
   );
 }
+
